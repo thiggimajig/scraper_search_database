c800f3104a5e967195a717e8e389f680dd5fd88f
Keira <higgins.taylor.a@gmail.com>
2018-02-13 10:25:39 -0500

    added pandas and throttling
    to do is set up cron job
    and postgres tutorial on personal computer 
    all so can feed files into noselenium and then into xpath.

--- DDAScraper.py
+++ DDAScraper.py
@@ -1,4 +1,4 @@
-
+#step 1 
 
 #find all a links DONE
 #(ideally only with d or d/a)
@@ -6,6 +6,14 @@
 #put into an array DONE ish they are in a csv
 #go to next page DONE
 #repeat DONE
+#
+#2/1/18 update:
+#scrape the file links and put into csv
+#take that link 
+#(after I need to clean it) cleaned in noSelenium.py
+#and adjust so correct final link
+#scrape that final link and print necessary info to csv 
+#(need to create a loop on all links) 
 
 #use pandas to analyze form type D or D/A against link? if desperate..
 
@@ -19,6 +27,9 @@ from lxml import html
 import re
 import csv
 from bs4 import BeautifulSoup
+import pandas as pd
+
+
 #array started to append all links I scrape into it to then go to noSelenium
 startUrlArray = []
 
@@ -26,8 +37,17 @@ startUrlArray = []
 f = csv.writer(open("DDAscrapings.csv", "w"))
 f.writerow(["link", "time"])
 #made an array of all sec pages
-secPages = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=0','https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=100','https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=200','https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=300','https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=400']
+secPages = ['https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=0']#,'https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=100']#,'https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=200','https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=300','https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=400']
 #will loop through each one and request the data and put into soup variable
+
+#to be polite and not slam the sec server
+import time
+for pg in secPages:
+    page = requests.get(pg)
+       
+    time.sleep(5)  # wait 5 seconds before we make the next request
+
+
 for pg in secPages:
 #first page, maybe loop through 1 to 5 after defining them all 
 #sec_page_1 = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=0'
@@ -37,15 +57,66 @@ for pg in secPages:
 #sec_page_5 = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcurrent&datea=&dateb=&company=&type=D&SIC=&State=&Country=&CIK=&owner=include&accno=&count=100&start=400'
 
 	page = requests.get(pg)
-
+	
 	soup = BeautifulSoup(page.content, 'html.parser')
 	soup.prettify()
 	#print(soup.prettify())
+	#
+	#trying to only scrape D/A and D
+	#soup.find("div", id="search-results").find_all("a", "external-links"):
+	#for element in soup.find('td', {'nowrap': "nowrap"}).find_all('a'):
+
+		
 	for element in soup.findAll('a'):
-		#links = element
-		#print(links)
+
+    	#links = element
+		print(element)	
 		f.writerow([element, datetime.now()]) #WORKS! I now have all a's in csv
 	#add to DDAscrapings.csv so I can see if I'm getting what I need
+
+#try with pandas
+#pandas needs to receive a list or put index in for scalar values 
+#https://stackoverflow.com/questions/17839973/construct-pandas-dataframe-from-values-in-variables
+
+
+#TO DO
+#do this next http://newcoder.io/scrape/part-3/ to set up postgres on personal computer
+#Schedule for each day (using cron job https://en.wikipedia.org/wiki/Cron#Predefined_scheduling_definitions ) 
+	
+		longlinks = pd.DataFrame({
+				"links": [element],
+			#"datetime": [datetime.now()]
+ 			})
+		print(longlinks)
+
+#df = pd.read_csv('DDAscrapings.csv', sep=',')
+#print(df)
+
+
+#perhaps helpful for specifiying D and D/A
+#from https://stackoverflow.com/questions/45653935/web-scraping-both-html-text-and-image-link-with-python-beautifulsoup?rq=1
+# player_data = []
+# for tr in data_rows:
+#     tdata = []
+#     for td in tr:
+#         tdata.append(td.getText())
+
+#         if td.div and td.div['class'][0] == 'school-logo':
+#             tdata.append(td.div.a['href'])
+
+#     player_data.append(tdata)
+
+
+
+
+		#tried and failed here to denest the a links i get returned in links so i can strip and add to a clean list to be looped through. 
+		#maybe I just need to try cleaning/trimming first
+		# for i in links:
+		# 	if isinstance(i, list):
+		# 		startUrlArray.extend(i)
+		# 	else:
+		# 		startUrlArray.append(i)
+		# print(startUrlArray)
 
 # for element in soup.findAll('td', attrs={'nowrap': 'nowrap'}):
 # 	tag = element
